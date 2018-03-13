eb75947fc1d1cdc6e77faacbe42f865524d9a098
Katarina Tokolyova <katarina.tokolyova@gmail.com>
2018-03-13 15:52:37 +0100

    Var chat

---
+++ app.js
@@ -0,0 +1,9 @@
+var Chat = (function () {
+      // Users
+      let users = []
+      let module = {}
+      module.joinChat = function (user) {
+          users.push(user)
+}
+      return module
+})()
